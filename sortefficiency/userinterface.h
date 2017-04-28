ecca24428b172e73d1769a1919a5f161fa78366d
mike <mike.shamilov@gmail.com>
2017-04-27 23:54:42 -0400

    (bug) elementSize = 1 after sort is run

--- sortefficiency/userinterface.h
+++ sortefficiency/userinterface.h
@@ -7,49 +7,72 @@
 
 using std::cin;
 
+
+
 class UserInterface {
+private:
+	bool menu, game, generate, sort, stats;
+	int elementSize;
 	
-public:
+	int arrayList[100000] = {0};
+	TestList tlist;
+	SortAlgs salgs;
+	
 	void mainMenu();
-	void newData();
-	void sortData();
-	void showStatistics();	
+	void generateMenu();
+	void newData(int);
+	void sortdataMenu();
+	void sortData(int);
+	void runBubbleSort(int);
+public:
+	UserInterface();
+	void runTests();
 };
 
-
+UserInterface::UserInterface() {
+	elementSize = 3;
+	menu = true;
+	game = true; 
+	generate = false;
+	sort = false;
+	stats = false;
+}
 
 void UserInterface::mainMenu() {
 	int choice;
 	cout << "Sort Experiments\n"
 		<< "--------------------\n\n";
-	cout << "(1) Generate New Data\n" // newData function
+	cout << "(1) Generate Data\n" // newData function
 		<< "(2) Sort Data\n" // sortData function
 		<< "(3) Show Statistics\n" // showStatistics function ??, save to txt
 		<< "(4) QUIT\n>> ";
-	
 	cin >> choice;
 	
-	switch (choice) {
+	switch(choice) {
 		case 1:
-			newData();
+			menu = false;
+			generate = true;
 			break;
 		case 2:
-			sortData();
+			menu = false;
+			sort = true;
 			break;
 		case 3:
-			//showStatistics();
+			menu = false;
+			stats = true;
 			break;
 		case 4:
+			menu = false;
+			game = false;
 			break;
-		
 		default:
-			cout << "not a valid entry\n";
-			mainMenu();
-	}		
+			cout << "invalid entry\n";
+			break;
+	}
 }
 
-void UserInterface::newData() {
-	int choice[2];
+void UserInterface::generateMenu() {
+	int choice;
 	
 	cout << "Test Data\n"
 		<< "-------------\n\n";
@@ -58,52 +81,54 @@ void UserInterface::newData() {
 		<< "(3) Average Case\n" // random
 		<< "(4) All\n"
 		<< "(5) Main Menu\n>> ";
-	cin >> choice[0];
-	if(choice[0] == 5) {
-		mainMenu();
-	}
-	if( (choice[0] != 1) && (choice[0] != 2) && (choice[0] != 3) && (choice[0] != 4) ) {
-			cout << "not a valid entry\n";
-			newData();
-	}
-	cout << "How many data elements to generate?\n>> ";
-	cin >> choice[1];
-	cout << choice[1];
+	cin >> choice;
 	
-	switch (choice[0]) {
-		case 1:
-			// generate best list 
+		switch(choice) {
+		case 1 ... 4:
+			newData(choice);
 			break;
-		case 2:
-			// generate worst list
-			break;
-		case 3:
-			// generate average list
-			break;
-		case 4:
-			// generate all 3
+		case 5:
+			menu = true;
+			generate = false;
 			break;
 		default:
-			cout << "not a valid entry\n";
-			newData();
-	}	
-}
-
-void UserInterface::sortData() {
-	int choice[2];
-	
-	cout << "Type of data to use?\n"
-		<< "-------------\n\n";
-	cout << "(1) Best Case\n" //
-		<< "(2) Worst Case\n" //
-		<< "(3) Average Case\n" //
-		<< "(4) All\n"
-		<< "(5) Main Menu\n>> ";
-	cin >> choice[0];
-	if(choice[0] == 5) {
-		mainMenu();
+			cout << "invalid entry\n";
+			break;
 	}
 	
+}
+
+void UserInterface::newData(int choice) {
+	cout << "Enter amount of data\n>> ";
+	cin >> elementSize;
+	
+	switch(choice) {
+		case 1:
+			tlist.bestCase(elementSize);
+			cout << "best_case.txt created with " << elementSize << " elements\n";
+			break;
+		case 2:
+			tlist.worstCase(elementSize);
+			cout << "worst_case.txt created with " << elementSize << " elements\n";
+			break;
+		case 3:
+			tlist.averageCase(elementSize);
+			cout << "average_case.txt created with " << elementSize << " elements\n";
+			break;
+		case 4:
+			tlist.bestCase(elementSize);
+			cout << "best_case.txt created with " << elementSize << " elements\n";
+			tlist.worstCase(elementSize);
+			cout << "worst_case.txt created with " << elementSize << " elements\n";
+			tlist.averageCase(elementSize);
+			cout << "average_case.txt created with " << elementSize << " elements\n";
+			break;
+	}
+	generateMenu();	
+}
+
+void UserInterface::sortdataMenu() {
+	int choice;
 	cout << "Type of sort?\n"
 		<< "-------------\n\n";
 	cout << "(1) Bubble\n" // 
@@ -113,19 +138,113 @@ void UserInterface::sortData() {
 		<< "(5) Quick\n" //
 		<< "(6) All (Warning! may take a long time)\n"
 		<< "(7) Main Menu\n>> ";
-	cin >> choice[1];
-	cout << choice[1];
-	if(choice[1] == 7) {
-		mainMenu();
-	}
+	cin >> choice;
+		
+	switch(choice) {
+		case 1 ... 6:
+			sortData(choice);
+			break;
+		case 7:
+			menu = true;
+			sort = false;
+			break;
+		default:
+			cout << "invalid entry\n";
+			break;
+	}	
+}
+
+void UserInterface::sortData(int choice) {
 	
-	if( (choice[0] > 0 && choice[0] < 5) && (choice[1] > 0 && choice[1] < 7) ){
-		cout << "ok\n";
-		// run sort funtion
-	} else {
-		cout << "not a valid entry\n";
-		sortData();
+	int testChoice;
+	
+	cout << "Select Test Case\n"
+		<< "-------------\n\n";
+	cout << "(1) Best Case\n" // 1...n
+		<< "(2) Worst Case\n" // n...1
+		<< "(3) Average Case\n" // random
+		<< "(4) All\n"
+		<< "(5) Main Menu\n>> ";
+	cin >> testChoice;
+	
+	switch(choice) {
+		
+		case 1:
+			cout << "\telement size = " << elementSize << endl; // 
+			runBubbleSort(testChoice);
+			cout << "\telement size = " << elementSize << endl;
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
 	}
 }
 
+void UserInterface::runBubbleSort(int testChoice) {
+	double runTime;
+	switch(testChoice) {
+		case 1:
+			tlist.listToArray(0, arrayList);
+			std::cout << "start best case" << std::endl;				
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";
+			break;
+		case 2:
+			tlist.listToArray(1, arrayList);
+			cout << "start worst case" << std::endl;				
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";				
+			break;
+		case 3:
+			tlist.listToArray(2, arrayList);
+			std::cout << "start average case" << std::endl;
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";
+			break;
+		case 4:
+			tlist.listToArray(0, arrayList);
+			std::cout << "start best case" << std::endl;				
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";
+			
+			tlist.listToArray(1, arrayList);
+			std::cout << "start worst case" << std::endl;		
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";
+	
+			tlist.listToArray(2, arrayList);
+			std::cout << "start average case" << std::endl;		
+			runTime = salgs.bubbleSort(elementSize, arrayList);
+			cout << runTime << " seconds\n";
+	
+			break;
+		case 5:
+			menu = true;
+			sort = false;
+			break;
+		default:
+			cout << "invalid entry\n";
+			break;
+	}
+}
+
+void UserInterface::runTests() {
+	while(game) {
+		while(menu) {
+			mainMenu();
+		}
+		while(generate) {
+			generateMenu();
+		}
+		while(sort) {
+			sortdataMenu();
+		}	
+	}
+}
 #endif
